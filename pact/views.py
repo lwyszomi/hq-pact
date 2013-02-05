4f61813159d17bb09f65256f88166c8199c6a337
Mike White <m@mwhite.info>
2013-02-05 15:30:17 -0500

    dimagi.utils.web.render_to_response -> django.shortcuts.render

--- pact/views.py
+++ pact/views.py
@@ -3,9 +3,9 @@ from corehq.apps.domain.decorators import login_and_domain_required
 from corehq.apps.reports.views import require_case_view_permission
 from corehq.apps.reports import util
 from corehq.apps.reports.standard import inspect
-from dimagi.utils.web import   render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.shortcuts import render
 from casexml.apps.case.models import CommCareCase
 from couchdbkit.exceptions import ResourceNotFound
 #from fields import FilterUsersField
@@ -28,7 +28,7 @@ def pact_case_details(request, domain, case_id):
 
     report_name = 'Details for Case "%s"' % case.name
 #    form_lookups = dict((form.get_id, "%s: %s" % (form.received_on.date(), xmlns_to_name(domain, form.xmlns, get_app_id(form)))) for form in case.get_forms())
-    return render_to_response(request, "reports/reportdata/case_details.html", {
+    return render(request, "reports/reportdata/case_details.html", {
         "domain": domain,
         "case_id": case_id,
 #        "form_lookups": form_lookups,
